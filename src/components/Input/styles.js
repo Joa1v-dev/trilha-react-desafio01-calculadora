484c64db1e3761e7e4d05c39b0a439e790e0395a
Joa1v-dev <devjoa1v@gmail.com>
2025-12-03 20:28:55 -0300

    Implementação das operações que faltavam

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const InputContainer = styled.div`
     width: 100%;
     height: 75px;
-    background-color: #AAAAFF;
+    background-color: #038dc4ff;
 
     display: flex;
     align-items: center;
@@ -16,11 +16,12 @@ export const InputContainer = styled.div`
     input {
         width: 100%;
         height: 75px;
-        background-color: #AAAAFF;
+        background-color: #0e5977ff;
         border: 0;
         display: flex;
         flex-direction: column;
         align-items: flex-end;
+        text-align: right;
         padding: 0 10px;
         font-size: 24px;
         font-family: 'Roboto';
